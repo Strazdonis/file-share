1e2804ccee7bb41c6e6ea3ec2059ca988d3067a7
Strazdonis <edvinasstrazdonis@gmail.com>
2020-10-25 01:36:52 +0300

    successfuly send 1 chunk files to server in tcp_connector, clean up package.json

--- tcp_connector.js
+++ tcp_connector.js
@@ -1,6 +1,9 @@
 const net = require('net');
-
+const fs = require('fs');
 const readline = require('readline');
+
+const CHUNK_SIZE = 10 * 1024 * 1024, // 10MB
+    buffer = Buffer.alloc(CHUNK_SIZE);
 
 function askQuestion(query) {
     const rl = readline.createInterface({
@@ -14,22 +17,91 @@ function askQuestion(query) {
     }));
 }
 
-async function askAndSend(client) {
-    const ans = await askQuestion("Data to send:");
-    //255 indicates start of a chunk
-    const string = new Uint8Array([255, ...ans.split("").map((char) => char = char.charCodeAt(0).toString(10).padStart(2, "0"), "")]);
-    console.log(string);
-    //chunk size
-    const size = new Uint8Array([string.length, 0, 0, 0]);
+function getFilesizeInBytes(filename) {
+    var stats = fs.statSync(filename);
+    var fileSizeInBytes = stats.size;
+    return fileSizeInBytes;
+}
+
+const START = new Uint8Array([255]);
+function encode(buf) {
+    return Uint8Array.from(buf);
+}
+
+
+async function sendFile(client) {
+    const filePath = await askQuestion("File to send:");
+    let file_size = getFilesizeInBytes(filePath);
+    file_size += Math.ceil(file_size / CHUNK_SIZE); //add up for '255' in the beggining of every chunk
+    const chunk_amount = Math.ceil(file_size / CHUNK_SIZE);
+
+    //4 seems to work, not sure about bigger ones.
+    const size = new Uint8Array(chunk_amount > 4 ? chunk_amount : 4);
+    let index = 0;
+    for (let i = file_size > CHUNK_SIZE ? CHUNK_SIZE : file_size; i <= file_size; i += CHUNK_SIZE) {
+        size[index] = i;
+        index += 1;
+    }
     console.log(size);
+
     client.write(size);
-    setTimeout(() => {
-        client.write(string, err => {
-            console.log(err);
-            askAndSend(client);
-        });
-        
-    }, 50);
+
+    fs.open(filePath, 'r', function (err, fd) {
+
+        if (err) {
+            throw err;
+        }
+        function readNextChunk() {
+            //buffer[0] = 255;
+
+            fs.read(fd, buffer, 0, CHUNK_SIZE, null, function (err, nread) {
+                // i assume this needs to be +1 since im adding 255 to the start of every chunk.
+                nread += 1;
+                console.log("NR:", nread);
+                console.log("BUFF", buffer);
+                if (err) {
+                    throw err;
+                }
+
+                if (nread === 1) {
+                    // done reading file, do any necessary finalization steps
+
+                    fs.close(fd, function (err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                    return sendFile(client);
+                }
+
+                let data;
+                if (nread < CHUNK_SIZE) {
+                    let slice = buffer.slice(0, nread-1); //subtract the added 1 (L:56)
+                    console.log(slice);
+                    data = encode(Buffer.concat([START, slice]));
+                }
+
+                else {
+                  
+                    data = encode(Buffer.concat([START, buffer]));
+                }
+                console.log("SENDING", data);
+                
+                client.write(data, err => {
+                    if (err) {
+                        throw err;
+                    }
+                });
+
+
+
+                readNextChunk();
+            });
+
+        }
+        readNextChunk();
+
+    });
 
 }
 
@@ -40,18 +112,21 @@ async function init() {
         ip = "127.0.0.1:55755";
     }
     console.log("Attempting to connect...");
-    client.connect(55755, '127.0.0.1', async function () {
+    client.connect(55755, '127.0.0.1', function () {
         console.log('Connected');
-        askAndSend(client);
+        sendFile(client);
     });
 
     client.on('data', function (data) {
         console.log('Received: ' + data);
-
     });
 
     client.on('close', function () {
         console.log('Connection closed');
+    });
+    
+    client.on('error', function (err) {
+        console.log('Connection threw an error', err);
     });
 }
 
